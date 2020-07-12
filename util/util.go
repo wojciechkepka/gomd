56fbe0f6362f4d8b18e98e6cb959886a866822ec
wojciechkepka <wojtek.kepka@protonmail.com>
2020-07-12 15:29:37 +0200

    Add util module

---
+++ util/util.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	rt "runtime"
+)
+
+func UrlOpen(url string) error {
+	var cmd *exec.Cmd
+
+	switch os := rt.GOOS; os {
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "linux":
+		cmd = exec.Command("xdg-open", url)
+	case "windows":
+		cmd = exec.Command("start", url)
+	default:
+		return fmt.Errorf("Unsupported os %v", os)
+	}
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	return cmd.Run()
+}
