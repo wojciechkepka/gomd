4eb541a6dc7657978df399fa1e7d77e36bc261e5
wojciechkepka <wojtek.kepka@protonmail.com>
2020-07-12 15:18:13 +0200

    Update building process

--- Makefile
+++ Makefile
@@ -1,13 +1,4 @@
 this:
-	mkdir -v ./static/css || echo
-	sassc ./sass/fv_common.sass ./static/css/fv_common.css
-	sassc ./sass/fv_dark.sass ./static/css/fv_dark.css
-	sassc ./sass/fv_light.sass ./static/css/fv_light.css
-	sassc ./sass/ghmd.sass ./static/css/ghmd.css
-	sassc ./sass/ghmd_dark.sass ./static/css/ghmd_dark.css
-	sassc ./sass/ghmd_light.sass ./static/css/ghmd_light.css
-	sassc ./sass/fonts.scss ./static/css/fonts.css
-	sassc ./sass/style.sass ./static/css/style.css
 	./scripts/add_css.sh
 	go build .
 
--- scripts/add_css.sh
+++ scripts/add_css.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+################################################################################
+# Compile css
+
+mkdir -v ./static/css 2>/dev/null || true
+
+command -v sass >/dev/null
+if [ $? != 0 ]
+then
+    command -v sassc >/dev/null
+    if [ $? != 0 ]
+    then
+        echo "Build failed. No sass or sassc in $PATH"
+        exit 1
+    else
+        SASS=sassc
+    fi
+else
+    SASS=sass
+fi
+
+$SASS ./sass/fv_common.sass ./static/css/fv_common.css
+$SASS ./sass/fv_dark.sass ./static/css/fv_dark.css
+$SASS ./sass/fv_light.sass ./static/css/fv_light.css
+$SASS ./sass/ghmd.sass ./static/css/ghmd.css
+$SASS ./sass/ghmd_dark.sass ./static/css/ghmd_dark.css
+$SASS ./sass/ghmd_light.sass ./static/css/ghmd_light.css
+$SASS ./sass/fonts.scss ./static/css/fonts.css
+$SASS ./sass/style.sass ./static/css/style.css
+
+
+################################################################################
+# Write to .go file
 GO_FILE="./mdserver/html/css.go"
 
 echo -e "package html\n\n" > $GO_FILE
