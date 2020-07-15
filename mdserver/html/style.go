41a665918f14a207445764e4209acd7c0c414294
wojciechkepka <wojtek.kepka@protonmail.com>
2020-07-15 09:16:51 +0200

    Add different top bar based on darkMode

--- mdserver/html/style.go
+++ mdserver/html/style.go
@@ -3,17 +3,17 @@ package html
 //MdFileStyle returns style for markdown file
 func MdFileStyle(isDarkMode bool, theme string) string {
 	if isDarkMode {
-		return StyleBeg + Fonts + Ghmd + GhmdDark + Style + ThemeCSS(isDarkMode, theme) + NL + StyleEnd
+		return StyleBeg + Fonts + Ghmd + GhmdDark + Style + TopBarDark + ThemeCSS(isDarkMode, theme) + NL + StyleEnd
 	}
 
-	return StyleBeg + Fonts + Ghmd + GhmdLight + Style + ThemeCSS(isDarkMode, theme) + NL + StyleEnd
+	return StyleBeg + Fonts + Ghmd + GhmdLight + Style + TopBarLight + ThemeCSS(isDarkMode, theme) + NL + StyleEnd
 }
 
 //FileListViewStyle returns style for main file list view
 func FileListViewStyle(isDarkMode bool) string {
 	if isDarkMode {
-		return StyleBeg + Fonts + FvCommon + FvDark + Style + NL + StyleEnd
+		return StyleBeg + Fonts + FvCommon + FvDark + Style + TopBarDark + NL + StyleEnd
 	}
 
-	return StyleBeg + Fonts + FvCommon + FvLight + Style + NL + StyleEnd
+	return StyleBeg + Fonts + FvCommon + FvLight + Style + TopBarLight + NL + StyleEnd
 }
