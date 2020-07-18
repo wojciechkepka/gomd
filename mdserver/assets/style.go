d2d716af4549f4900889efe6d961b907e40747d6
Wojciech Kępka <wojtek.kepka@protonmail.com>
2020-07-18 17:19:36 +0200

    Fix all lints

--- mdserver/assets/style.go
+++ mdserver/assets/style.go
@@ -3,17 +3,17 @@ package assets
 //MdFileStyle returns style for markdown file
 func MdFileStyle(isDarkMode bool, theme string) string {
 	if isDarkMode {
-		return Fonts + Ghmd + GhmdDark + Css + TopBarDark + ThemeCSS(isDarkMode, theme)
+		return Fonts + Ghmd + GhmdDark + CSS + TopBarDark + ThemeCSS(isDarkMode, theme)
 	}
 
-	return Fonts + Ghmd + GhmdLight + Css + TopBarLight + ThemeCSS(isDarkMode, theme)
+	return Fonts + Ghmd + GhmdLight + CSS + TopBarLight + ThemeCSS(isDarkMode, theme)
 }
 
 //FileListViewStyle returns style for main file list view
 func FileListViewStyle(isDarkMode bool) string {
 	if isDarkMode {
-		return Fonts + FvCommon + FvDark + Css + TopBarDark
+		return Fonts + FvCommon + FvDark + CSS + TopBarDark
 	}
 
-	return Fonts + FvCommon + FvLight + Css + TopBarLight
+	return Fonts + FvCommon + FvLight + CSS + TopBarLight
 }
