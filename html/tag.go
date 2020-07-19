3de69648afd514612d2ebc1475b004d9bd974f7a
Wojciech Kępka <wojtek.kepka@protonmail.com>
2020-07-19 09:04:09 +0200

    cleanup html module

--- html/tag.go
+++ html/tag.go
@@ -1,5 +1,7 @@
 package html
 
+import "strings"
+
 //TagName is an enum with all allowed HTML tags.
 type TagName string
 
@@ -23,6 +25,75 @@ const (
 
 	Doctype = "<!DOCTYPE html>"
 )
+
+//Tag is a struct for creating custom html tags
+type Tag struct {
+	Type       TagName
+	content    string //Content is only appliable when it has a closing tag.
+	attributes map[string]string
+	hasClosing bool //whether this tag has a closing tag required.
+}
+
+//NewTag initializes a new instance of CustomTag
+func NewTag(name TagName) Tag {
+	return Tag{
+		Type:       name,
+		content:    "",
+		attributes: make(map[string]string),
+		hasClosing: true,
+	}
+}
+
+//AddAttr adds an attribute to this tag's map
+func (t *Tag) AddAttr(k, v string) {
+	t.attributes[k] = v
+}
+
+//HasClosingTag sets whethet this tag has a closing tag
+func (t *Tag) HasClosingTag(val bool) {
+	t.hasClosing = val
+}
+
+//SetContent sets content of this tag. Content will only be
+//visible if this tag has a closing tag so the content can
+//be placed in between the tags.
+func (t *Tag) SetContent(content string) {
+	t.content = content
+}
+
+//Render renders this tag as html adding all attributes and content
+//and enclosing it in tags.
+func (t *Tag) Render() string {
+	s := strings.Builder{}
+	s.WriteRune('<')
+	s.WriteString(string(t.Type))
+	s.WriteRune(' ')
+	for attr, val := range t.attributes {
+		s.WriteString(attr)
+		s.WriteString("=\"")
+		s.WriteString(val)
+		s.WriteString("\" ")
+	}
+	s.WriteRune('>')
+
+	if t.hasClosing {
+		s.WriteString(t.content)
+		s.WriteString("</")
+		s.WriteString(string(t.Type))
+		s.WriteRune('>')
+	}
+
+	return s.String()
+}
+
+//Render renders this tag as html adding all attributes and content
+//and enclosing it in tags.
+func Render(t Tag) string {
+	return t.Render()
+}
+
+//################################################################################
+// Funcs for tag creation
 
 //Title returns a title string enclosed in title tags
 func Title(title string) Tag {
