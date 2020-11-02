a3bf56693a8c925b1bcedbf431d4b1c6765d38ca
wojciechkepka <wojtek.kepka@protonmail.com>
2020-11-02 17:01:47 +0100

    Some comments on mdserver/template

--- mdserver/template/tmpl.go
+++ mdserver/template/tmpl.go
@@ -8,14 +8,20 @@ import (
 	ttmpl "text/template"
 )
 
+// HTemplate is an interface grouping all HTML templates together
+// providing functionality like RenderHString
 type HTemplate interface {
 	Template() (*htmpl.Template, error)
 }
 
+// TTemplate is an interface grouping all text templates together
+// providing functionality like RenderHString
 type TTemplate interface {
 	Template() (*ttmpl.Template, error)
 }
 
+// HTemplateFromBox returns a HTML template with name set to name,
+// found in a file within a box specified by path
 func HTemplateFromBox(path, file, name string) (*htmpl.Template, error) {
 	box := packr.NewBox(path)
 	f, err := box.FindString(file)
@@ -29,6 +35,8 @@ func HTemplateFromBox(path, file, name string) (*htmpl.Template, error) {
 	return tmpl, nil
 }
 
+// RenderHTemplate renders html template to string logging errors and
+// returning empty string by default
 func RenderHTemplate(tmpl *htmpl.Template, obj interface{}) string {
 	buf := []byte{}
 	w := bytes.NewBuffer(buf)
@@ -39,6 +47,8 @@ func RenderHTemplate(tmpl *htmpl.Template, obj interface{}) string {
 	return w.String()
 }
 
+// RenderHString grabs an HTML template specified by templ interface
+// and renders it using that object.
 func RenderHString(templ HTemplate) string {
 	tmpl, err := templ.Template()
 	if err != nil {
@@ -48,10 +58,13 @@ func RenderHString(templ HTemplate) string {
 
 }
 
+// RenderHTML returns a rendered template as template.HTML
 func RenderHTML(templ HTemplate) htmpl.HTML {
 	return htmpl.HTML(RenderHString(templ))
 }
 
+// TTemplateFromBox returns a text template with name set to name,
+// found in a file within a box specified by path
 func TTemplateFromBox(path, file, name string) (*ttmpl.Template, error) {
 	box := packr.NewBox(path)
 	f, err := box.FindString(file)
@@ -65,6 +78,8 @@ func TTemplateFromBox(path, file, name string) (*ttmpl.Template, error) {
 	return tmpl, nil
 }
 
+// RenderTTemplate renders text template to string logging errors and
+// returning empty string by default
 func RenderTTemplate(tmpl *ttmpl.Template, obj interface{}) string {
 	buf := []byte{}
 	w := bytes.NewBuffer(buf)
@@ -75,6 +90,8 @@ func RenderTTemplate(tmpl *ttmpl.Template, obj interface{}) string {
 	return w.String()
 }
 
+// RenderTString grabs a text template specified by templ interface
+// and renders it using that object.
 func RenderTString(templ TTemplate) string {
 	tmpl, err := templ.Template()
 	if err != nil {
